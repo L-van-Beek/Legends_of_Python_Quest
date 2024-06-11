ed1bba047d718c8a885b06105e9f45ec1d7ff76d
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-06-11 16:15:28 +0200

    Create 3_detention.py

---
+++ Loops/3_detention.py
@@ -0,0 +1,7 @@
+
+
+#Task: Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.Create a detention.py program that does this using code.
+
+#code: 
+for i in range(100):
+  print("I will not use snapchat in class")
