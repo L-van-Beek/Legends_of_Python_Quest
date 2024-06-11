f470a35ca50dea45a201ed85a977ddb5ecd3771e
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-06-11 13:53:12 +0200

    Create 1_enter_the_pin.py

---
+++ Control_Flow/Loops/1_enter_the_pin.py
@@ -0,0 +1,11 @@
+# Learning to use the loop with the keyword "while" 
+
+print('BANK OF LJUBOV')
+
+pin = int(input('Enter your PIN: '))
+
+while pin != 1234:
+  pin = int(input('Incorrect PIN. Enter your PIN again: '))
+
+if pin == 1234:
+  print('PIN accepted!')
