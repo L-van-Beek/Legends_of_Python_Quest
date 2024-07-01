1411813d5d1532e501c5be7039a25a12edbe0781
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-01 15:49:27 +0200

    Create bucket_list.py

---
+++ Lists/bucket_list.py
@@ -0,0 +1,21 @@
+# Task: My Bucket list 
+# Create a bucket_list.py program with your own unique bucket list.
+# A. First, create a things_to_do list, and add things you want to do in your lifetime.
+# A. 
+things_to_do = [
+  '🚀 Create the dopest learn to code platform ever.',
+  '⛰️ Hike the Pacific Crest Trail.',
+  '🏡 Build an A-frame house and raise some goats.',
+  '🌏 Live somewhere in Asia for a year.',
+  '🎸 Release an album.',
+  '📝 Write a book.',
+  '🏆 Reach 100k subscribers on YouTube.',
+  '🚐 Road trip with the fam.',
+  '🍳 Open a cozy diner upstate.',
+  '👴🏻 Grow old with no regrets.'
+]
+# B. Now, iterate over the list and print everything out.
+
+print(things_to_do)
+for i in range(len(things_to_do)): # B. 
+  print(things_to_do[i])
