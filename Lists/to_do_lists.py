dab58f1bab0414182aa1f8aaab68e7a298460735
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-01 14:42:06 +0200

    Create to_do_lists.py

---
+++ Lists/to_do_lists.py
@@ -0,0 +1,24 @@
+# Write code below 💖
+
+# Create a todo.py program that will define a todo list that contains the following items:
+
+# '🏦 Get quarters.'
+# '🧺 Do laundry.'
+# '🌳 Take a walk.'
+# '💈 Get a haircut.'
+# '🍵 Make some tea.'
+# '💻 Complete Lists chapter.'
+# '💖 Call mom.'
+# '📺 Watch My Hero Academia.'
+
+# A. Print the first item and the second item. What did you get?
+# B. Next, use slicing to print the third, fourth, and fifth items.
+
+
+# Code: 
+to_do = ['🏦 Get quarters.', '🧺 Do laundry.', '🌳 Take a walk.', '💈 Get a haircut.', '🍵 Make some tea.', '💻 Complete Lists chapter.', '💖 Call mom.', '📺 Watch My Hero Academia.']
+# A. 
+print(to_do[0:2]) 
+# B. 
+print(to_do[2:5])
+
