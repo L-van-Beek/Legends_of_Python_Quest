74c50d5de4147cd82f2d1e58b82c918470ac8436
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-01 14:27:38 +0200

    Create groceries.py

---
+++ Lists/groceries.py
@@ -0,0 +1,6 @@
+# Task
+
+# Create a grocery.py program that with a grocery list of things that you need to get from the store:
+
+grocery = ['🥚 Eggs', '🥑 Avocados', '🍪 Cookies', '🌶 Hot Pepper Jam', '🫐  Bluberries', '🥦 Broccoli']
+print(grocery)
