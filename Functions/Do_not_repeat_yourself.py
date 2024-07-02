ffabe6d777af3be39127ac3d061b6fb388701a80
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-02 15:00:46 +0200

    Create Do_not_repeat_yourself.py

---
+++ Functions/Do_not_repeat_yourself.py
@@ -0,0 +1,26 @@
+# Assignment: 
+# Create a dry.py program and check out the complete list of built-in functions:
+
+# Find 4 built-in functions that we have used previously in the course.
+# Pick 1 built-in function that we have not seen before.
+# Use each of these once in the program.
+
+# For the new function, try to read the documentation (😵‍💫) or Google it (👍)!
+
+# Add a comment for each built-in function to explain how they work.
+
+# Code: 
+# First function introduced in the course: 
+print('Hello, user!')
+# Another function I learned was:
+user_kid1 = input('How old is your first child? \n')
+user_kid2 = input('How old are your second child? \n')
+# Third function transforms input into nummerical value (integer)
+kid1 = int(user_kid1)
+kid2 = int(user_kid2)
+# and function number 4 calculates the maximum amongst the numbers
+old = max(user_kid1, user_kid2)
+print('Your oldest child is' +  (old)  + 'years old')
+# New function that I have not seen before is: 
+import random 
+# It imports the library with random numbers 
