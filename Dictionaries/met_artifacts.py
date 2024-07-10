53413be8f4ab698c7bdcb44e229225410a4d72ff
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-10 14:20:49 +0200

    Create met_artifacts.py

---
+++ Dictionaries/met_artifacts.py
@@ -0,0 +1,7 @@
+# Creating a dictionary
+met_artifacts = {'title': 'Harpsichord', 'date': 'late 17th century', 'geography': 'Italy', 'culture': 'Italian', 'medium': 'Wood, paint, various materials', 'dimensions': 'Instr. 244cm x 90cm x 31cm front stand 82.5cm x 22cm x 78cm back stand 21cm x 21cm x 78cm', 'classifications': 'Chordophone-Zither-plucked-harpsichord, Paintings', 'credit_line': 'Gift of Susan Dwight Bliss, 1945', 'accession_number': '45.41a-c'}
+
+# Accessing dictionary elements
+print('Title:', met_artifacts['title'])
+print('Date:', met_artifacts['date'])
+print('Origin:', met_artifacts['geography'])
