ebbc23a818efc505a50c7381b469b96796ef41d8
Ljubov van Beek <ljubov.van.beek@gmail.com>
2024-07-12 10:38:22 +0200

    Create Upd_dict.py

---
+++ Data_structures/Upd_dict.py
@@ -0,0 +1,7 @@
+# futball team Netherlands 
+# Task: create a dictionary eith five futball players of dutch team, plying for champiogship (include jersey number, goals scored, games played, position. 
+# explore statidtics: 
+#Code: 
+
+futball_players = [{}, {}, {}, {}, {}]
+futball_players.append() 
